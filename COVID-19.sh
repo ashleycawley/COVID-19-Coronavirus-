b62dcf4caea7b7c1ea57ab50f7cf137ddec40150
Ashley Cawley <ash@ashleycawley.co.uk>
2020-03-14 12:07:37 +0000

    Updated to support new data format of https://ncov2019.live/

--- COVID-19.sh
+++ COVID-19.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Author: Ashley Cawley / ash@ashleycawley.co.uk / @ashleycawley on Twitter
+# Retrieves data from the superb website: https://ncov2019.live/
+
+# Variables
 COUNTRY="United Kingdom"
 DATE=`date '+%A %d %B %Y at %T'`
-RETRIEVED_DATA=`curl -s https://ncov2019.live/ | html2text | grep -A 1 -i "$COUNTRY" | grep -v "$COUNTRY"`
+CONFIRMED=`curl -s https://ncov2019.live/ | html2text | grep -i "$COUNTRY" | awk '{print $3}' | uniq`
+DECEASED=`curl -s https://ncov2019.live/ | html2text | grep -i "$COUNTRY" | awk '{print $4}' | uniq`
+RECOVERED=`curl -s https://ncov2019.live/ | html2text | grep -i "$COUNTRY" | awk '{print $5}' | uniq`
+SERIOUS=`curl -s https://ncov2019.live/ | html2text | grep -i "$COUNTRY" | awk '{print $6}' | uniq`
 
+# Main Output
 echo "
-COVID-19 (Coronavirus) Infections in $COUNTRY
-$DATE
-$RETRIEVED_DATA"
+COVID-19 (Coronavirus) Infections in $COUNTRY on $DATE
+Confirmed: $CONFIRMED | Deceased: $DECEASED | Recovered: $RECOVERED | Serious: $SERIOUS"
 
 exit 0
